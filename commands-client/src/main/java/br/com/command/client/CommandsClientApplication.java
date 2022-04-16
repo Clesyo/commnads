1da7d8d49a6b72d713a125c4d8864041e440aae9
Clesyo <clesyo.silva@gmail.com>
2022-04-16 08:32:10 -0300

    Criando projeto de cliente

---
+++ commands-client/src/main/java/br/com/command/client/CommandsClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.command.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandsClientApplication.class, args);
+	}
+
+}
